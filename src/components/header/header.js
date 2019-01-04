b4d4733c25796f1c2896516c00181a099c7e3b78
unknown <C:\Users\administrator\AppData\Roaming\The Bat!>
2019-01-04 17:12:23 +0300

    Bootstrap not worked

---
+++ src/components/header/header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { PageHeader } from 'react-bootstrap';
+
+    class Header extends React.Component{
+        render(){
+            return (
+<PageHeader>
+  Example page header <small>Subtext for header</small>
+</PageHeader>;
+           );
+        }   
+    }
+    export default Header;
