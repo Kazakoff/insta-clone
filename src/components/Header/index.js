7aea01d57f23493aa6f7baa1e9e4c300191f9fe3
Kazakoff <wadimchik@mail.ru>
2019-01-09 21:35:57 +0300

    Header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,15 +1,19 @@
 import React from "react";
 import { PageHeader } from "react-bootstrap";
+import logo from "../../img/logo.png";
 import "./Header.css";
 
 class Header extends React.Component {
   render() {
     return (
       <PageHeader>
-        {" "}
-        <a href="/">
-          <img src="logo.png" alt="Insta-logo" /> Instagram
-        </a>
+        <nav className="haeder-logo">
+          <a href="/">
+            <img className="logo" src={logo} alt="logo" />
+          </a>
+        </nav>
+        <nav className="header-title"> Instagram </nav>
+        <nav className="header-user"> k@mail.ru </nav>
       </PageHeader>
     );
   }
