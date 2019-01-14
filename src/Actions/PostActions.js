b4ec1d9e47f1a78d931e4dbb5e758089ad6fbe10
Kazakoff <wadimchik@mail.ru>
2019-01-14 22:07:28 +0300

    Redux start

---
+++ src/Actions/PostActions.js
@@ -0,0 +1,15 @@
+import { ADD_POST, DELETE_POST } from "../Data/ActionsTypes";
+export const createPost = ({ title, body }) => ({
+  type: ADD_POST,
+  payload: {
+    title,
+    body
+  }
+});
+
+export const deletePost = id => ({
+  type: DELETE_POST,
+  payload: {
+    id
+  }
+});
