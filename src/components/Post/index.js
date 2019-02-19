e0b9b879978ecf12a0982371b49b5d21291fc0c3
Kazakoff <wadimchik@mail.ru>
2019-02-19 22:31:54 +0300

    styles -w

--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -2,11 +2,22 @@ import React from "react";
 import "./Post.css";
 import PropTypes from "prop-types";
 import { ListGroupItem } from "react-bootstrap";
-import styled from "@emotion/styled";
+// import styled from "@emotion/styled";
+import { css } from "@emotion/core";
 
+const StyledPost = css`
+  display: flex;
+  flex-direction: row;
+  border-radius: 3px;
+  border: 1px solid #e6e6e6;
+  background-color: #fff;
+  margin-bottom: 60px;
+  margin-left: 20%;
+  margin-right: 20%;
+`;
 const Post = ({ post, onDelete }) => {
   return (
-    <ListGroupItem className="Post">
+    <ListGroupItem className={StyledPost}>
       <div className="Post-image">
         {post.pic !== "" ? <img alt={post.caption} src={post.pic} /> : ""}
       </div>
