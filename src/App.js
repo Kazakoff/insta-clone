33718d1f530b8530451b38c6ce80d05e13f1643a
Kazakoff <wadimchik@mail.ru>
2019-08-02 13:31:30 +0300

    Themes, composition fixed

--- src/App.js
+++ src/App.js
@@ -1,27 +1,26 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { BrowserRouter, Route } from 'react-router-dom';
+import { ThemeProvider } from 'emotion-theming';
 import UserView from './views/UserView';
 import AdminView from './views/AdminView';
 import store from './redux/store/store';
 import EntranceView from './views/Entrance';
+import { theme } from './styles';
 // <Router history={browserHistory}>
-/*
-        <Route exact path="/" component={EntranceView} />
-        <Route path="/login" component={EntranceView} />
-        <Route path="/posts" component={UserView} />
-          <Route path="/admin" component={AdminView} />
-        */
+
 const App = () => (
   <div>
-    <Provider store={store}>
-      <BrowserRouter>
-        <Route exact path="/" component={EntranceView} />
-        <Route path="/login" component={EntranceView} />
-        <Route path="/posts" component={UserView} />
-        <Route path="/admin" component={AdminView} />
-      </BrowserRouter>
-    </Provider>
+    <ThemeProvider theme={theme}>
+      <Provider store={store}>
+        <BrowserRouter>
+          <Route exact path="/" component={EntranceView} />
+          <Route path="/login" component={EntranceView} />
+          <Route path="/posts" component={UserView} />
+          <Route path="/admin" component={AdminView} />
+        </BrowserRouter>
+      </Provider>
+    </ThemeProvider>
   </div>
 );
 
--- src/views/Entrance/components/styles.js
+++ src/views/Entrance/components/styles.js
@@ -2,37 +2,4 @@
 import styled from '@emotion/styled';
 import { jsx, css } from 'emotion';
 
-// border-color: rgb(23, 60, 53);
-
-export const basicStyle = css`
-  border-color: red;
-  background-color: red;
-`;
-
-export const BigButton = styled.button`
-  ${basicStyle};
-  background-color: red;
-  font-weight: 700;
-  text-decoration: none;
-  user-select: none;
-  padding: 2em 2em;
-  margin: 2em;
-  outline: none;
-  border: 2px solid;
-  border-radius: 2px;
-  transition: 0.2s;
-`;
-export const CenteredButtonToolbar = styled.div`
-  display: flex;
-  border-color: rgb(82, 60, 53);
-  align-items: center;
-  justify-content: center;
-  height: 100%;
-`;
-
-export const logo = css`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  height: 100%;
-`;
+export * from '../../../styles';
